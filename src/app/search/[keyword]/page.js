bb57f49ced6ef00d0bd4d5edd7974e5b88d66ccb
ryaand <bahriansyahherlambang3@gmail.com>
2026-06-28 18:44:43 +0700

    fix: decode keyword with no %20 now

--- src/app/search/[keyword]/page.js
+++ src/app/search/[keyword]/page.js
@@ -1,10 +1,23 @@
+import DefaultCard from "@/Components/Utilities/DefaultCard"
+import Link from "next/link"
+
 const Search = async ({ params }) => {
 
-    const {keyword} = await params
+    const { keyword } = await params
+    const respon = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}&limit=5`)
+    const search = await respon.json()
 
-    return(
+    console.log(search)
+    return (
         <div>
-            <div>ini {keyword}</div>
+            <div>ini {decodeURIComponent(keyword)}</div>
+            <div className="grid grid-cols-5">
+                {search.data.map(data => (
+                    <Link href={`/Anime/${data.mal_id}`} key={data.mal_id}>
+                        <DefaultCard key={data.mal_id} title={data.title} age={data.rating} images={data.images.webp.image_url} review={data.score} reviewer={data.scored_by}></DefaultCard>
+                    </Link>
+                ))}
+            </div>
         </div>
     )
 }
